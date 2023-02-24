792d192a43a57dda29ab1ef4837060b7885a09d4
HACAS <hamsa.casale@hotmail.com>
2023-02-24 17:52:04 +0100

    rettet opp i error

--- ressursarkiv/src/App.js
+++ ressursarkiv/src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Main from './components/Main';
 import Navigation from './components/Navigation';
+import Resources from './components/Resources';
 import './css/sass/main.scss';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <>
     <Header />
     <Navigation />
-    <Main />
+    <Resources />
     </>
     )
   }
--- ressursarkiv/src/components/Navigation.js
+++ ressursarkiv/src/components/Navigation.js
@@ -1,9 +1,9 @@
-import Html from "./Html";
+
 
 export default function Navigation(){
     return(
     <nav>
-        <button id="HTML"><Html/></button>
+        <button id="HTML">HTML</button>
         <button id="CSS">CSS</button>
         <button id="JavaScript">JavaScript</button>
         <button id="React">React</button>
