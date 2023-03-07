f86e5c4e51c5a2624134de1297610ad6e94deed9
Cibzo <Cibzo@outlook.com>
2023-03-07 19:45:36 +0100

    .

--- ressursarkiv/src/App.js
+++ ressursarkiv/src/App.js
@@ -1,33 +1,32 @@
-import { Route, Routes } from 'react-router-dom';
-import Error from './components/Error';
-import Header from './components/Header';
-import Html from './components/Html';
-import JavaScript from './components/JavaScript';
-import Main from './components/Main';
-import Navigation from './components/Navigation';
-import React from './components/React';
-import Resources from './components/Resources';
-import Sanity from './components/Sanity';
-import './css/sass/main.scss';
-import { resources } from './data/ressurser';
-
-
+import { Route, Routes } from "react-router-dom";
+import Css from "./components/Css";
+import Error from "./components/Error";
+import Header from "./components/Header";
+import Html from "./components/Html";
+import JavaScript from "./components/JavaScript";
+import Main from "./components/Main";
+import Navigation from "./components/Navigation";
+import React from "./components/React";
+import Resources from "./components/Resources";
+import Sanity from "./components/Sanity";
+import "./css/sass/main.scss";
+import { resources } from "./data/ressurser";
 
 function App() {
   return (
     <>
-    <Header title={"Resussarkiv"} />
-    <Navigation />
-    <Routes>
-      <Route index path='/html' element={<Html />} />
-      <Route path='/javascript' element={<JavaScript />} />
-      <Route path='/react' element={<React />} />
-      <Route path='/headless-cms' element={<Sanity />}/>
-      <Route path='*' element={<Error />} />
-    </Routes>
-    
+      <Header title={"Resussarkiv"} />
+      <Navigation />
+      <Routes>
+        <Route index path="/html" element={<Html />} />
+        <Route path="/javascript" element={<JavaScript />} />
+        <Route path="/react" element={<React />} />
+        <Route path="/headless-cms" element={<Sanity />} />
+        <Route path="/css" element={<Css />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </>
-    )
-  }
+  );
+}
 
 export default App;
