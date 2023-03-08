39a3d5e0f5ee403405b778ed9c760ce3b7afdf5f
mistamambo <mister.mambo@hotmail.com>
2023-03-08 17:03:41 +0100

    Fikset egen component for Linker

--- ressursarkiv/src/App.js
+++ ressursarkiv/src/App.js
@@ -1,24 +1,14 @@
-import { Route, Routes } from "react-router-dom";
-import Css from "./components/Css";
 import Header from "./components/Header";
-import Html from "./components/Html";
-import JavaScript from "./components/JavaScript";
+import Links from "./components/Links";
 import Navigation from "./components/Navigation";
-import React from "./components/React";
-import Sanity from "./components/Sanity";
+
 
 function App() {
   return (
     <>
       <Header title={"Resussarkiv"} />
       <Navigation />
-      <Routes>
-        <Route index path="/html" element={<Html />} />
-        <Route path="/javascript" element={<JavaScript />} />
-        <Route path="/react" element={<React />} />
-        <Route path="/headless-cms" element={<Sanity />} />
-        <Route path="/css" element={<Css />} />
-      </Routes>
+      <Links/>
     </>
   );
 }
