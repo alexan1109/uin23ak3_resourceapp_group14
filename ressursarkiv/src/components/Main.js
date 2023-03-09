975436c11cf06e75b9d7aef8f60b1ffe1e1328ba
Cibzo <Cibzo@outlook.com>
2023-03-09 08:31:10 +0100

    småfikset koden

--- ressursarkiv/src/components/Main.js
+++ ressursarkiv/src/components/Main.js
@@ -1,19 +1,17 @@
 import Resources from "./Resources";
 import Title from "./Title";
 
-export default function Main(){
-    return (
-        <>
-        <div className="container">
+export default function Main() {
+  return (
+    <>
+      <div className="container">
         <main>
-            <article id="htmlinfo">
-                <Title className="category_name" name="HTML"/>
-                <ul >
-                    <Resources/>
-                </ul>
-            </article>
+          <article id="htmlinfo">
+            <Title className="category" />
+            <Resources />
+          </article>
         </main>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
--- ressursarkiv/src/components/Resources.js
+++ ressursarkiv/src/components/Resources.js
@@ -1,12 +1,15 @@
 import { resources } from "../data/ressurser";
 
-
-export default function Resources({id}){
-    return
-        <>         
-        {resources.map((item)=>(
-            item.category === id ? 
-            <li><a href={item.url}>{item.title}</a></li>:null))}
-        </>
-
+export default function Resources({ id }) {
+  return (
+    <ul>
+      {resources.map((item) =>
+        item.category === id ? (
+          <li>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ) : null
+      )}
+    </ul>
+  );
 }
